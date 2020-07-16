af300c7ae8cc5c2b0d50cdfb4a002a9a805f52ff
Parth <parthparmaronline@gmail.com>
2020-07-16 11:47:51 +0530

    open door rotator

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -19,7 +19,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("THIS IS SPARTTAA! on %s"), *ObjectName);
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at, %s"), *ObjectName, *ObjectPos);
 	
 }
 
